019b8d9c3335bc6eaa1e51a2c12df68ec38d9fd0
Patty Eckhart <pdraney@vols.utk.edu>
2019-02-15 11:10:20 -0500

    script to generate collar data

---
+++ data/generateData.js
@@ -0,0 +1,45 @@
+const uuidv1 = require('uuid/v1');
+var fs = require('fs'); 
+var datetime = require('node-datetime');
+
+var data = []
+for(var i=0; i < 100; i++){
+    var now = Date.now()
+    var sortKey = datetime.create(now)
+    data.push(
+        {
+            "partitionKey" : uuidv1(),
+            "sortKey" : sortKey._created,
+            "motion" : {
+                    "x" : 123,
+                    "y" : 456,
+                    "z" : 789
+                },
+            "gps" : {
+                "latitude": {
+                    "direction": "N",
+                    "degrees": i,
+                    "minutes": Math.abs(60-i),
+                    "seconds": Math.abs(60-i)/30
+                },
+                "longitude" : {
+                    "direction": "E",
+                    "degrees": 360-i,
+                    "minutes": Math.abs(40-i),
+                    "seconds": Math.abs(15-i)/30
+                } 
+            },
+            "audio" : [2000,2050,2500,2000,1850]
+        }
+    )
+}
+
+const content = JSON.stringify(data);
+
+fs.appendFile('./data/collerData.json', content, 'utf8', function (err) {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
